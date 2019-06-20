e87d1ccbcfefdca44789eac41782aba69e4a48e2
Sascha Koch <sascha.koch@web.de>
2019-06-20 15:01:47 +0200

    basic rocket app added

--- rust-greenhouse/src/main.rs
+++ rust-greenhouse/src/main.rs
@@ -1,3 +1,17 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/sensor/<name>")]
+#[response(status = 200, content_type = "json")]
+fn get_sensor_data(name: String) -> String {
+  "[16, 19, 21.2, 20.2, 19.2, 18.7]"
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  rocket::ignite()
+    .mount("/greenhouse", routes![get_sensor_data])
+    .launch();
 }
