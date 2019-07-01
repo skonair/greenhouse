9546a14d00ce2b4a7275b3be027b3d79af27f7d0
Sascha Koch <sascha.koch@web.de>
2019-07-02 00:17:58 +0200

    some playing around with rust

--- raspi-greenhouse/rust-consolidate/src/main.rs
+++ raspi-greenhouse/rust-consolidate/src/main.rs
@@ -1,67 +1,68 @@
 extern crate chrono;
 
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
-use chrono::format::ParseError;
-
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
-use std::io::BufReader;
 use std::io::BufRead;
+use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::Write;
 use std::time::{Duration, SystemTime};
 
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+use chrono::format::ParseError;
+use std::ptr::hash;
+use chrono::offset::TimeZone;
+use itertools::Itertools;
 
+#[derive(Debug)]
+struct Measure {
+    chunk: i64,
+    value: f32,
+}
 
-fn main() {   
-  let mut writer = BufWriter::new(io::stdout());
-  let now = Utc::now();
-  writeln!(writer, "Now is {:?}", now);
+fn main() {
+    let mut writer = BufWriter::new(io::stdout());
+    let now = Utc::now();
+    writeln!(writer, "Now is {:?}", now);
 
-  let duration = 1440;
-  let delta = 5;
-  let max_chunks = 288;
+    let duration = 1440;
+    let delta = 5;
+    let max_chunks = 288;
 
-  let f = File::open("./data/test.txt").unwrap();
-  let file = BufReader::new(&f);
-  let mut last_chunk = 0;
-  let mut hash_map = HashMap::new();
-  for (num, line) in file.lines().enumerate() {
-    let l = line.unwrap();
+    let f = File::open("./data/test_t1.txt").unwrap();
+    let file = BufReader::new(&f);
+    let mut ve: Vec<Measure> = vec![];
+    for (num, line) in file.lines().enumerate() {
+        let l = line.unwrap();
 
-    let mut split = l.split(" ");
-    let v: Vec<&str> = split.collect();
-    let mut iter = v.chunks(2);
+        let v: Vec<&str> = l.split(" ").collect();
+        let sdt = format!("{} {}", v[0], v[1]);
+        let result = v[3].to_string().parse::<f32>().unwrap();
 
-    let dt = iter.next().unwrap();
-    writeln!(writer, "date found {}", dt[0]);
-    writeln!(writer, "time found {}", dt[1]);
-    let sdt = format!("{} {}", dt[0], dt[1]);
-    writeln!(writer, "Sdt is -{}-", &sdt);
+        let custom = NaiveDateTime::parse_from_str(&sdt, "%Y-%m-%d %T");
+        let utc_custom = DateTime::<Utc>::from_utc(custom.unwrap(), Utc);
+        let duration = now.signed_duration_since(utc_custom);
+        let chunk_no = duration.num_minutes() / delta;
 
-    let custom = NaiveDateTime::parse_from_str(&sdt, "%Y-%m-%d %T");
-    writeln!(writer, "Custom value is {:?}", custom);
-
-    let utcCustom = DateTime::<Utc>::from_utc(custom.unwrap(), Utc);
-
-    let duration = now.signed_duration_since(utcCustom);
-    writeln!(writer, "Duration: {:?}", duration);
-    writeln!(writer, "As whole minutes: {:?}", duration.num_minutes());
-
-    let chunk_no = duration.num_minutes() / delta;
-    writeln!(writer, "chunk_no is {}", chunk_no);
-
-    if (chunk_no > last_chunk) {
-      // write out hashmap
-
-      hash_map.clear();
+        let measure = Measure { chunk: chunk_no, value: result };
+        ve.push(measure);
     }
 
-    for elem in iter {
-      writeln!(writer, "Key {}, Value {}", elem[0], elem[1]);
-      hash_map.insert(elem[0], elem[1]);
-    }
+    let x = ve.iter()
+        .group_by(|m| m.chunk)
+        .into_iter()
+        .map(|(ge0, group)| (ge0, mean(group.collect())))
+        .collect::<Vec<(i64, f32)>>();
 
-  }       
+    println!("{:?}", x);
+
+
+
+}
+
+
+fn mean(numbers: Vec<&Measure>) -> f32 {
+    let sum: f32 = numbers.iter().map(|m| m.value).sum();
+    sum as f32 / numbers.len() as f32
 }
