df6d57c3eff3a63243766c5b1095663c36562117
Sascha Koch <sascha.koch@web.de>
2019-07-02 11:59:58 +0200

    debug info added

--- raspi-greenhouse/cpp/receive_sensor_data.cpp
+++ raspi-greenhouse/cpp/receive_sensor_data.cpp
@@ -37,14 +37,14 @@ int main(int argc, char** argv) {
   radio.begin();
   radio.setAutoAck(false);
   radio.setRetries(15,15);
-  radio.setPALevel(RF24_PA_LOW); // default is RF24_PA_MAX
+  radio.setPALevel(RF24_PA_HIGH); // default is RF24_PA_MAX
 
   radio.openWritingPipe(addresses[1]);
   radio.openReadingPipe(1,addresses[0]);
 
   radio.startListening();
 
-  //radio.printDetails();
+  radio.printDetails();
 
   while (1) {
     // if there is data ready
